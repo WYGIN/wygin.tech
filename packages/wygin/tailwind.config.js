c120452eb7a33124f676aa8276f8ee52a1c4d93a
WYGIN <wygininc@gmail.com>
2023-06-09 08:13:04 +0000

    added flowbite flowbite-react & ran prettier

--- packages/wygin/tailwind.config.js
+++ packages/wygin/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
