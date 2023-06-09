4e2130e8026cddeb8a2a56399d8e4935530826b5
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-06-09 21:40:43 +0530

    miving buttton from components to mantine

--- packages/wygin/src/components/Button/Button.tsx
+++
@@ -1,7 +0,0 @@
-/** @jsxImportSource react */
-import type { ButtonProps, ButtonGroupProps } from "@mantine/core";
-import { Button as FbButton } from "@mantine/core";
-import { qwikify$ } from "@builder.io/qwik-react";
-
-export const Button = qwikify$<ButtonProps>(FbButton);
-export const ButtonGroup = qwikify$<ButtonGroupProps>(FbButton.Group);
