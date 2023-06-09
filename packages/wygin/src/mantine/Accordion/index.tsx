eb8ef9e383ed0b6b4d265b94e6f3b3a9c5d7eadc
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-06-09 22:02:18 +0530

    Created Accordion

---
+++ packages/wygin/src/mantine/Accordion/index.tsx
@@ -0,0 +1,9 @@
+/** @jsxImportSource react */
+import type { AccordionProps, AccordionControlProps, AccordionItemProps, AccordionPanelProps } from "@mantine/core";
+import { Accordion as QwikAccordion } from "@mantine/core";
+import { qwikify$ } from "@builder.io/qwik-react";
+
+export const Accordion = qwikify$<AccordionProps>(QwikAccordion);
+export const AccordionControl = qwikify$<AccordionControlProps>(QwikAccordion.Control);
+export const AccordionItem = qwikify$<AccordionItemProps>(QwikAccordion.Item);
+export const AccordionPanel = qwikify$<AccordionPanelProps>(QwikAccordion.Panel);
