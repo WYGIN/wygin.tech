18319012665169959dc8013f964b36918c07c1cb
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-06-10 13:34:01 +0530

    Update HeaderMegaMenuLayout.tsx

--- packages/wygin/src/layouts/HeaderMegaMenuLayout.tsx
+++ packages/wygin/src/layouts/HeaderMegaMenuLayout.tsx
@@ -98,103 +98,14 @@ export interface HeaderMegaMenuLayoutProps {
   showSearchIcon?: boolean;
   showAuthButtons?: boolean;
   showUserProfileAvatar?: boolean;
-  navLinks: {
-    icon: HTMLElement & SVGElement;
-    href: string;
-    label: string;
-    megaMenu?: {
-      viewAll: {
-        label: string;
-        href: string;
-      }
-      getStartedLabel: string;
-      getStartedDescription: string;
-      getStartedButtonLabel: string;
-      megaMenuItems: {
-        icon: HTMLElement & SVGElement;
-        title: string;
-        description: string;
-      }[];
-    };
-  }[];
+  megaNavbar: HTMLElement;
+  drawer: HTMLElement;
 }
 
-export function HeaderMegaMenu({ logo, showThemeToggle, showSearchIcon, showAuthButtons, showUserProfileAvatar, navLink }: HeaderMegaMenuLayoutProps) {
+export function HeaderMegaMenu({ logo, showThemeToggle, showSearchIcon, showAuthButtons, showUserProfileAvatar, megaNavbar, drawer }: HeaderMegaMenuLayoutProps) {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
   const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
   const { classes, theme } = useStyles();
-
-  const NavLinksElement = navLinks.map((navLink) => (
-    {
-      navLink?.megaMenu
-      ? (
-          <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-            <HoverCard.Target>
-              <a href={navLink.href} className={classes.link}>
-                <Center inline>
-                  <Box component="span" mr={5}>
-                    {navLink.label}
-                  </Box>
-                  <IconChevronDown size={16} color={theme.fn.primaryColor()} />
-                </Center>
-              </a>
-            </HoverCard.Target>
-
-            <HoverCard.Dropdown sx={{ overflow: 'hidden' }}>
-              <Group position="apart" px="md">
-                <Text fw={500}>{navLink.label}</Text>
-                <Anchor href={navLink.megaMenu.viewAll.href} fz="xs">
-                  {navLink.megaMenu.viewAll.label}
-                </Anchor>
-              </Group>
-
-              <Divider
-                my="sm"
-                mx="-md"
-                color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'}
-              />
-
-              <SimpleGrid cols={2} spacing={0}>
-                {
-                  navLink.megaMenu.megaMenuItems.map((megaMenuItem) => (
-                    <UnstyledButton className={classes.subLink} key={megaMenuItem.title}>
-                      <Group noWrap align="flex-start">
-                        <ThemeIcon size={34} variant="default" radius="md">
-                          <megaMenuItem.icon size={rem(22)} color={theme.fn.primaryColor()} />
-                        </ThemeIcon>
-                        <div>
-                          <Text size="sm" fw={500}>
-                            {megaMenuItem.title}
-                          </Text>
-                          <Text size="xs" color="dimmed">
-                            {megaMenuItem.description}
-                          </Text>
-                        </div>
-                      </Group>
-                    </UnstyledButton>
-                  ));
-                }
-              </SimpleGrid>
-
-              <div className={classes.dropdownFooter}>
-                <Group position="apart">
-                  <div>
-                    <Text fw={500} fz="sm">
-                      {navLink.megaMenu.getStartedLabel}
-                    </Text>
-                    <Text size="xs" color="dimmed">
-                      {navLink.megaMenu.getStartedDescription}
-                    </Text>
-                  </div>
-                  <Button variant="default">{navLink.megaMenu.getStartedButtonLabel}</Button>
-                </Group>
-              </div>
-            </HoverCard.Dropdown>
-          </HoverCard>
-        )
-      : <a href={navLink.href} key={navLink.href} className={classes.link}>{navLink.label}</a>
-    }
-  ));
 
   return (
     <Box pb={120}>
@@ -203,7 +114,7 @@ export function HeaderMegaMenu({ logo, showThemeToggle, showSearchIcon, showAuth
           <MantineLogo size={30} />
 
           <Group sx={{ height: '100%' }} spacing={0} className={classes.hiddenMobile}>
-            {navLink.NavLinkElement}        
+            {megaNavbar}        
           </Group>
 
           <Group className={classes.hiddenMobile}>
@@ -226,26 +137,7 @@ export function HeaderMegaMenu({ logo, showThemeToggle, showSearchIcon, showAuth
       >
         <ScrollArea h={`calc(100vh - ${rem(60)})`} mx="-md">
           <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
-
-          <a href="#" className={classes.link}>
-            Home
-          </a>
-          <UnstyledButton className={classes.link} onClick={toggleLinks}>
-            <Center inline>
-              <Box component="span" mr={5}>
-                Features
-              </Box>
-              <IconChevronDown size={16} color={theme.fn.primaryColor()} />
-            </Center>
-          </UnstyledButton>
-          <Collapse in={linksOpened}>{links}</Collapse>
-          <a href="#" className={classes.link}>
-            Learn
-          </a>
-          <a href="#" className={classes.link}>
-            Academy
-          </a>
-
+            {drawer}
           <Divider my="sm" color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'} />
 
           <Group position="center" grow pb="xl" px="md">
@@ -258,4 +150,4 @@ export function HeaderMegaMenu({ logo, showThemeToggle, showSearchIcon, showAuth
   );
 }
 
-export const HeaderMegaMenuLayout = qwikify$<HeaderMegaMenuLayoutProps>(HeaderMegaMenu);
+export const HeaderMegaMenuLayout = qwikify$<HeaderMegaMenuLayoutProps>(HeaderMegaMenu, { eagerness: 'visible' });
