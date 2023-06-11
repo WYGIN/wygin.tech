d4c79a1b6cda2132020047ef789df1e427842bfb
WYGIN <wygininc@gmail.com>
2023-06-11 13:59:47 +0000

    fixed status schema error

--- packages/drizzle-db/src/schema/status.ts
+++ packages/drizzle-db/src/schema/status.ts
@@ -1,5 +1,5 @@
 import { mysqlEnum } from "drizzle-orm/mysql-core";
 import { z } from 'zod';
 
-export const status = mysqlEnum('status', ['published', 'draft', 'scheduled', 'archived']).default('PUBLISHED').notNull().default('published');
+export const status = mysqlEnum('status', ['published', 'draft', 'scheduled', 'archived']).default('published').notNull().default('published');
 export const zodStatus = z.enum(['published', 'draft', 'scheduled', 'archived'])
